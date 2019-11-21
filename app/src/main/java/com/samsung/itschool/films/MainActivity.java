7fbadfdc7d81a28eb17de8414838e04b36681a25
Ivan <ivan.kiel@gmail.com>
2019-11-21 16:31:45 +0800

    random film

--- app/src/main/java/com/samsung/itschool/films/MainActivity.java
+++ app/src/main/java/com/samsung/itschool/films/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     String[] movies;
+    boolean [] shownMovies;
     TextView tvTitle;
 
     @Override
@@ -19,14 +20,26 @@ public class MainActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.title);
         // загружаем ресурсы
         Resources r = getResources();
+
         movies = r.getStringArray(R.array.movies);
+        shownMovies = new boolean[movies.length];
+
     }
 
     public void nextClick(View v) {
-        tvTitle.setText(movies[0]);
+        // выбрать случайный номер фильма
+        int k = (int) (Math.random()*movies.length);
+        // если фильм был показан, сгенерировать новое
+        // случайное число, иначе отобразить название фильма
+        // и отметь, что фильм показан
+        // рекомендуется использовать цикл do-while
+
+        // если фильмы закончились, выдать сообщение об этом
+        // например, с помощью toast
+        // http://developer.alexanderklimov.ru/android/toast.php
     }
 
     public void resetClick(View v) {
-
+        // очистить или создать заново массив shownFilms
     }
 }
